b6fbe1d93dfb7e4de582a66208321a022c3b0a7f
Joe-REAL-0 <2018168257@qq.com>
2024-04-10 22:11:27 +0800

    完善用户账户检查逻辑，修复部分bug

--- __init__.py
+++ __init__.py
@@ -21,7 +21,7 @@ checkFish = on_command('查询鱼')
 checkUser = on_command('信息查询')
 
 def check_account(event):
-    user_id = event.sender.user_id
+    user_id = event.get_user_id()
     nickname = event.sender.nickname
     db = Database(user_id)
     db.checkAccount(nickname)
@@ -30,10 +30,12 @@ def check_account(event):
 
 @release.handle()
 async def release_handle(bot:Bot, event: Event, args:Message = CommandArg()):
-    user_id = event.sender.user_id
-    fishName = args.extract_plain_text()
+    user_id = event.get_user_id()
+    check_account(event)
+    args_sp = args.split(' ')
+    fishName = args_sp[0]
     try:
-        count = int(args.extract_plain_text())
+        count = int(args_sp[1])
     except:
         await release.finish("放生失败！\n*鱼的数量必须是个数字!")
     message = releaseMain(user_id, fishName, count)
@@ -41,39 +43,46 @@ async def release_handle(bot:Bot, event: Event, args:Message = CommandArg()):
 
 @fishing.handle()
 async def fishing_handle(bot:Bot, event: Event):
-    user_id = event.sender.user_id
+    check_account(event)
+    user_id = event.get_user_id()
     message = fishingMain(user_id)
     await fishing.finish(message)
 
 @upgrade.handle()
 async def upgrade_handle(bot:Bot, event: Event):
-    user_id = event.sender.user_id
+    check_account(event)
+    user_id = event.get_user_id()
     message = upgradeMain(user_id)
     await upgrade.finish(message)
 
 @listPool.handle()
 async def listPool_handle(bot:Bot, event: Event):
+    check_account(event)
     message = listFishMain()
     await listPool.finish(message)
 
 @leaderboard.handle()
-async def dashboard_handle(bot:Bot, event: Event):
+async def leaderboard_handle(bot:Bot, event: Event):
+    check_account(event)
     message = leaderboardMain()
     await leaderboard.finish(message)
 
 @checkFish.handle()
 async def checkFish_handle(bot:Bot, event: Event, args:Message = CommandArg()):
+    check_account(event)
     fishName = args.extract_plain_text()
     message = checkFishMain(fishName)
     await checkFish.finish(message)
 
 @checkUser.handle()
 async def checkUser_handle(bot:Bot, event: Event):
-    user_id = event.sender.user_id
+    check_account(event)
+    user_id = event.get_user_id()
     message = checkUserMain(user_id)
     await checkUser.finish(message)
 
 @help.handle()
 async def help_handle(bot:Bot, event: Event):
+    check_account(event)
     message = helpMain()
     await help.finish(message)   
