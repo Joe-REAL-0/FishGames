a1a9ad92db43d8a537ecc341da9d89965c40e052
Joe-REAL-0 <2018168257@qq.com>
2024-06-07 23:49:02 +0800

    修复放生鱼的bug

--- ReleaseFish.py
+++ ReleaseFish.py
@@ -7,7 +7,7 @@ fishReleaseCoolDownDict = {}
 def releaseMain(id,fishName):
     if not 0 < len(fishName) < 15:
         return "放生失败！\n*鱼的名称必须少于15字"
-    if len(db.selectFish(fishName)) != 0:
+    if FishDataManager().getFishByName(fishName) != None:
         return "放生失败！\n*鱼塘里已经存在这种鱼了！不能重复放生！"
     if id in fishReleaseCoolDownDict and fishReleaseCoolDownDict[id] > datetime.now():
         time_difference = fishReleaseCoolDownDict[id] - datetime.now()
@@ -17,6 +17,6 @@ def releaseMain(id,fishName):
     else:
         fishReleaseCoolDownDict[id] = datetime.now() + timedelta(hours=6)
     k=randint(0,3)
-    value = randint(20+k*30,50+k*30)
+    value = randint(15+k*30,20+k*30)
     FishDataManager().addFish(fishName, id, fishName , value)
     return f"放生成功！鱼塘因为 {fishName} 的加入变得更热闹了！\n目前这种鱼价值 {value} points/条"
