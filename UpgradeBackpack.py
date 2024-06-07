54183dc7349ab222af5a282dd4c75e17a3852dc0
Joe-REAL-0 <2018168257@qq.com>
2024-06-08 01:12:36 +0800

    修复了背包升级的文本错误

--- UpgradeBackpack.py
+++ UpgradeBackpack.py
@@ -7,7 +7,7 @@ def upgradeBackpackMain(id):
     point = db.selectPoint()
     pointNeeded = int((2.5**level) * 15)
     if point < pointNeeded:
-        message = f"升级鱼竿到Lv. {level+1} \n需要 {pointNeeded} points\n你的信用点不足"
+        message = f"升级背包到Lv. {level+1} \n需要 {pointNeeded} points\n你的信用点不足"
     else:
         message = f"你的背包成功升级\n Lv.{level} -> Lv. {level + 1}"
         message += f"\n背包容量增加至 {(level + 1) * 3 +1}"
