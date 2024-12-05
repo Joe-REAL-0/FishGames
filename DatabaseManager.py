f19a9530349591c0b83dc9b9a56809bb32300b0f
Joe-REAL-0 <2018168257@qq.com>
2024-12-05 17:26:14 +0800

    修改了占位符类型以及cursor的构造方式

--- DatabaseManager.py
+++ DatabaseManager.py
@@ -17,7 +17,7 @@ class Database:
             password = self.password,
             database = 'FishingGame'
         )
-        self.cursor = self.connection.cursor()
+        self.cursor = self.connection.cursor(prepared = True)
         self.cursor.execute('CREATE TABLE IF NOT EXISTS users (userId VARCHAR(128) PRIMARY KEY, name VARCHAR(128), point INTEGER)')
         self.cursor.execute('CREATE TABLE IF NOT EXISTS rod_levels (userId VARCHAR(128) PRIMARY KEY, level INTEGER)')
         self.cursor.execute('CREATE TABLE IF NOT EXISTS backpack_levels (userId VARCHAR(128) PRIMARY KEY, level INTEGER)')
@@ -30,9 +30,9 @@ class Database:
         
     def checkAccount(self,name):
         if self.userId == None: return
-        self.cursor.execute('SELECT * FROM users WHERE userId = ?', (self.userId,))
+        self.cursor.execute('SELECT * FROM users WHERE userId = %s', (self.userId,))
         if len(self.cursor.fetchall()) == 0:
-            self.cursor.execute('INSERT INTO users (userId, name, point) VALUES (?, ?, ?)', (self.userId ,name, 0))
+            self.cursor.execute('INSERT INTO users (userId, name, point) VALUES (%s, %s, %s)', (self.userId ,name, 0))
             self.connection.commit()
 
     # 鱼塘相关
@@ -41,34 +41,34 @@ class Database:
         self.cursor.execute('DELETE FROM pool')
         for data in fishData:
             if data[1] == 0: continue
-            self.cursor.execute('INSERT INTO pool (fishName, value, count, owner) VALUES (?, ?, ?, ?)', (data[0], data[1], data[2], data[3]))
+            self.cursor.execute('INSERT INTO pool (fishName, value, count, owner) VALUES (%s, %s, %s, %s)', (data[0], data[1], data[2], data[3]))
         self.connection.commit()
 
     def insertFish(self, fishName, value):
         if self.userId == None: return
-        self.cursor.execute('INSERT INTO pool (fishName, value, count, owner) VALUES (?, ?, ?, ?)', (fishName, value, 50, self.userId))
+        self.cursor.execute('INSERT INTO pool (fishName, value, count, owner) VALUES (%s, %s, %s, %s)', (fishName, value, 50, self.userId))
         self.connection.commit()
 
     def reduceFish(self, fishName):
-        self.cursor.execute('UPDATE pool SET count = count - 1 WHERE fishName = ?', (fishName,))
-        if self.cursor.execute('SELECT count FROM pool WHERE fishName = ?', (fishName,)).fetchone()[0] == 0:
-            self.cursor.execute('DELETE FROM pool WHERE fishName = ?', (fishName,))
+        self.cursor.execute('UPDATE pool SET count = count - 1 WHERE fishName = %s', (fishName,))
+        if self.cursor.execute('SELECT count FROM pool WHERE fishName = %s', (fishName,)).fetchone()[0] == 0:
+            self.cursor.execute('DELETE FROM pool WHERE fishName = %s', (fishName,))
         self.connection.commit()
 
     def removeFish(self, fishName):
-        self.cursor.execute('DELETE FROM pool WHERE fishName = ?', (fishName,))
+        self.cursor.execute('DELETE FROM pool WHERE fishName = %s', (fishName,))
         self.connection.commit()
 
     def selectFishOwner(self, fishName):
-        self.cursor.execute('SELECT owner FROM pool WHERE fishName = ?', (fishName,))
+        self.cursor.execute('SELECT owner FROM pool WHERE fishName = %s', (fishName,))
         result = self.cursor.fetchall()
         if len(result) == 0: return None, None
         userId = result[0][0]
-        self.cursor.execute('SELECT name FROM users WHERE userId = ?', (userId,))
+        self.cursor.execute('SELECT name FROM users WHERE userId = %s', (userId,))
         return userId,self.cursor.fetchall()[0][0]
     
     def selectFish(self,fishName):
-        self.cursor.execute('SELECT * FROM pool WHERE fishName = ?', (fishName,))
+        self.cursor.execute('SELECT * FROM pool WHERE fishName = %s', (fishName,))
         return self.cursor.fetchall()
 
     def selectPool(self):
@@ -79,7 +79,7 @@ class Database:
 
     def selectUser(self):
         if self.userId == None: return
-        self.cursor.execute('SELECT * FROM users WHERE userId = ?', (self.userId,))
+        self.cursor.execute('SELECT * FROM users WHERE userId = %s', (self.userId,))
         return self.cursor.fetchall()
     
     def selectAllUser(self):
@@ -88,21 +88,21 @@ class Database:
 
     def selectPoint(self):
         if self.userId == None: return
-        self.cursor.execute('SELECT point FROM users WHERE userId = ?', (self.userId,))
+        self.cursor.execute('SELECT point FROM users WHERE userId = %s', (self.userId,))
         return self.cursor.fetchall()[0][0]
     
     def changePoint(self, point):
         if self.userId == None: return
-        self.cursor.execute('UPDATE users SET point = point + ? WHERE userId = ?', (point, self.userId))
+        self.cursor.execute('UPDATE users SET point = point + %s WHERE userId = %s', (point, self.userId))
         self.connection.commit()
 
     #鱼竿相关
     
     def selectRodLevel(self):
         if self.userId == None: return
-        if len(self.cursor.execute('SELECT * FROM rod_levels WHERE userId = ?', (self.userId,)).fetchall()) == 0:
+        if len(self.cursor.execute('SELECT * FROM rod_levels WHERE userId = %s', (self.userId,)).fetchall()) == 0:
             self.insertRodLevel(1)
-        self.cursor.execute('SELECT * FROM rod_levels WHERE userId = ?', (self.userId,))
+        self.cursor.execute('SELECT * FROM rod_levels WHERE userId = %s', (self.userId,))
         return self.cursor.fetchall()[0][1]
     
     def selectAllRodLevel(self):
@@ -111,15 +111,15 @@ class Database:
     
     def insertRodLevel(self, level):
         if self.userId == None: return
-        self.cursor.execute('INSERT INTO rod_levels (userId, level) VALUES (?, ?)', (self.userId, level))
+        self.cursor.execute('INSERT INTO rod_levels (userId, level) VALUES (%s, %s)', (self.userId, level))
         self.connection.commit()
 
     def increaseRodLevel(self):
         if self.userId == None: return
         level = self.selectRodLevel()
         pointNeeded = int(exp(level-1)*20)
-        self.cursor.execute('UPDATE users SET point = point - ? WHERE userId = ?', (pointNeeded, self.userId))
-        self.cursor.execute('UPDATE rod_levels SET level = level + 1 WHERE userId = ?', (self.userId,))
+        self.cursor.execute('UPDATE users SET point = point - %s WHERE userId = %s', (pointNeeded, self.userId))
+        self.cursor.execute('UPDATE rod_levels SET level = level + 1 WHERE userId = %s', (self.userId,))
         self.connection.commit()
     
     #背包相关
@@ -127,14 +127,14 @@ class Database:
     def updateBackpack(self, backPackData):
         if self.userId == None: return
         dataString = json.dumps(backPackData)
-        self.cursor.execute('UPDATE backpack SET backPackData = ? WHERE userId = ?', (dataString, self.userId))
+        self.cursor.execute('UPDATE backpack SET backPackData = %s WHERE userId = %s', (dataString, self.userId))
         self.connection.commit()
     
     def selectBackpackLevel(self):
         if self.userId == None: return
-        if len(self.cursor.execute('SELECT * FROM backpack_levels WHERE userId = ?', (self.userId,)).fetchall()) == 0:
+        if len(self.cursor.execute('SELECT * FROM backpack_levels WHERE userId = %s', (self.userId,)).fetchall()) == 0:
             self.insertBackpackLevel(1)
-        self.cursor.execute('SELECT * FROM backpack_levels WHERE userId = ?', (self.userId,))
+        self.cursor.execute('SELECT * FROM backpack_levels WHERE userId = %s', (self.userId,))
         return self.cursor.fetchall()[0][1]
     
     def selectAllBackpackLevel(self):
@@ -143,23 +143,23 @@ class Database:
     
     def insertBackpackLevel(self, level):
         if self.userId == None: return
-        self.cursor.execute('INSERT INTO backpack_levels (userId, level) VALUES (?, ?)', (self.userId, level))
+        self.cursor.execute('INSERT INTO backpack_levels (userId, level) VALUES (%s, %s)', (self.userId, level))
         self.connection.commit()
     
     def increaseBackpackLevel(self):
         if self.userId == None: return
         level = self.selectBackpackLevel()
         pointNeeded = int((2.5**level) * 15)
-        self.cursor.execute('UPDATE users SET point = point - ? WHERE userId = ?', (pointNeeded, self.userId))
-        self.cursor.execute('UPDATE backpack_levels SET level = level + 1 WHERE userId = ?', (self.userId,))
+        self.cursor.execute('UPDATE users SET point = point - %s WHERE userId = %s', (pointNeeded, self.userId))
+        self.cursor.execute('UPDATE backpack_levels SET level = level + 1 WHERE userId = %s', (self.userId,))
         self.connection.commit()
 
     def selectBackpack(self):
         if self.userId == None: return
-        self.cursor.execute('SELECT * FROM backpack WHERE userId = ?', (self.userId,))
+        self.cursor.execute('SELECT * FROM backpack WHERE userId = %s', (self.userId,))
         result = self.cursor.fetchall()
         if len(result) == 0:
-            self.cursor.execute('INSERT INTO backpack (userId, backPackData) VALUES (?, ?)', (self.userId, '[]'))
+            self.cursor.execute('INSERT INTO backpack (userId, backPackData) VALUES (%s, %s)', (self.userId, '[]'))
             self.connection.commit()
             return []
         dataString = result[0][1]
